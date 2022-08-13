d057cf192c62591cac877a915cf350a997598756
Emmanuel Lozoya <jose.emmanuel.lozoya@gmail.com>
2022-08-12 20:58:27 -0500

    feat: add user bash shell support

--- src/ubuntu/common/add_user.sh
+++ src/ubuntu/common/add_user.sh
@@ -3,7 +3,8 @@
 # This user has a password an is sudoer
 # because I like to get inside the container and install new temporary tools just to play with them
 apt-get update
-apt-get -y install sudo zsh
+apt-get -y install sudo
+[ "$USER_SHELL" = "zsh" ] && apt-get -y install zsh
 groupadd -g $GROUP_ID $USER_GROUP_NAME
-useradd -rm -d /home/$USER_NAME -s /bin/zsh -g $USER_GROUP_NAME -G sudo -u $USER_ID $USER_NAME
+useradd -rm -d /home/$USER_NAME -s /bin/$USER_SHELL -g $USER_GROUP_NAME -G sudo -u $USER_ID $USER_NAME
 echo "$USER_NAME:$USER_PASS" | chpasswd
